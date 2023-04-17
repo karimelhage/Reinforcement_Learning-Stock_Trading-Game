a0ed3564e0d677808af12fd7da29e40f63bb7558
karimelhage <karimelhage94@gmail.com>
2023-04-17 17:00:17 +0200

    add scalar function

---
+++ env/calculations.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def get_scalar(input_array):
+    '''
+        Calculates the scale of a number. For example, the numbers 500,30, 0.2 have scales of 100,10, 0.1 respectively
+    '''
+
+    def find_scalar(input):
+        return 10 ** int("{:.2e}".format(input).split("e")[-1])
+
+    return np.vectorize(find_scalar, otypes=[float])(input_array)
